0e36454750daa33a970bc8ca21255efb5a9f8929
Anton Vusatyi <Anton_Vusatyi@vfc.com>
2023-02-02 23:02:48 +0300

    hw-1-11: Implement Service into jmp-cloud-service-impl. User Stream API

---
+++ jmp-cloud-service-impl/src/main/java/com/vusatui/jmp/cloud/service/impl/ServiceImpl.java
@@ -0,0 +1,30 @@
+package com.vusatui.jmp.cloud.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.vusatui.jmp.dto.BankCardDTO;
+import com.vusatui.jmp.dto.SubscriptionDTO;
+import com.vusatui.jmp.dto.UserDTO;
+import com.vusatui.jmp.service.api.Service;
+
+public class ServiceImpl implements Service {
+
+    private final List<UserDTO> userDTOS = new ArrayList<>();
+
+    @Override
+    public void subscribe(BankCardDTO bankCard) {
+        // Do smth
+    }
+
+    @Override
+    public Optional<SubscriptionDTO> getSubscriptionByBankCardNumber(String number) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return userDTOS;
+    }
+}
--- jmp-cloud-service-impl/src/main/java/module-info.java
+++ jmp-cloud-service-impl/src/main/java/module-info.java
@@ -1,2 +1,4 @@
 module com.vusatui.jmp.cloud.service.impl {
+    requires com.vusatui.jmp.service.api;
+    requires com.vusatui.jmp.dto;
 }
