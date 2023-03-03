a7b0a1d32201887be8afa54effa1092d8cfe4eb1
Anton Vusatyi <anton_vusatyi@pg.com>
2023-03-03 15:50:43 +0300

    DiskAnalyzer SearchForTheFileNameWithTheMaximumNumberOfLetters

---
+++ DiskAnalyzer/src/main/java/com/vusatui/Main.java
@@ -0,0 +1,20 @@
+package com.vusatui;
+
+import com.github.rvesse.airline.Cli;
+import com.github.rvesse.airline.help.Help;
+import com.vusatui.commands.SearchForTheFileNameWithTheMaximumNumberOfLetters;
+
+@com.github.rvesse.airline.annotations.Cli(
+        name = "sda",
+        description = "A simple disk analyzer",
+        defaultCommand = Help.class,
+        commands = { SearchForTheFileNameWithTheMaximumNumberOfLetters.class, Help.class }
+)
+public class Main {
+
+    public static void main(String[] args) {
+        Cli<Runnable> cli = new Cli(Main.class);
+        Runnable cmd = cli.parse(args);
+        cmd.run();
+    }
+}
