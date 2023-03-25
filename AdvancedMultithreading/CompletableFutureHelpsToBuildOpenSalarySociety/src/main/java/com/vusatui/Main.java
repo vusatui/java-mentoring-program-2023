6181a3a6cb47ee221302de1acbe0afe4d0b5b9ea
Anton Vusatyi <anton_vusatyi@pg.com>
2023-03-25 15:01:08 +0300

    completed  CompletableFutureHelpsToBuildOpenSalarySociety

--- AdvancedMultithreading/CompletableFutureHelpsToBuildOpenSalarySociety/src/main/java/com/vusatui/Main.java
+++ AdvancedMultithreading/CompletableFutureHelpsToBuildOpenSalarySociety/src/main/java/com/vusatui/Main.java
@@ -1,7 +1,21 @@
 package com.vusatui;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import com.vusatui.controller.EmployeeController;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        EmployeeController employeeController = new EmployeeController();
+        final CompletableFuture<Void> voidCompletableFuture = employeeController.getHiredEmployees()
+                .thenAccept(employeeDTOS -> employeeDTOS.forEach(employeeDTO -> {
+                    System.out.println(Thread.currentThread().getId());
+                    System.out.printf("%s %s %s %s %n", employeeDTO.getId(), employeeDTO.getName(),
+                            employeeDTO.getSurname(), employeeDTO.getSalary());
+                }));
+        System.out.println("Non blocked");
+        voidCompletableFuture.get();
     }
 }
--- AdvancedMultithreading/CompletableFutureHelpsToBuildOpenSalarySociety/src/main/java/com/vusatui/controller/EmployeeController.java
+++ AdvancedMultithreading/CompletableFutureHelpsToBuildOpenSalarySociety/src/main/java/com/vusatui/controller/EmployeeController.java
@@ -1,4 +1,37 @@
 package com.vusatui.controller;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+import com.vusatui.dto.EmployeeDTO;
+import com.vusatui.repository.EmployeeRepository;
+
 public class EmployeeController {
+
+    private static final EmployeeRepository employeeRepository = new EmployeeRepository();
+
+    public CompletableFuture<Collection<EmployeeDTO>> getHiredEmployees() {
+        return CompletableFuture.supplyAsync(() -> employeeRepository.hiredEmployees()
+                .thenCompose(employeeDTOS -> {
+                    final List<CompletableFuture<Void>> completableFutures = employeeDTOS.stream().map(
+                            employeeDTO -> employeeRepository.getSalary(employeeDTO.getId())
+                                    .thenAccept(salary -> {
+                                        System.out.println(employeeDTO.getId());
+                                        System.out.println(Thread.currentThread().getId());
+                                        try {
+                                            Thread.sleep(2000);
+                                        } catch (InterruptedException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                        employeeDTO.setSalary(salary);
+                                    })
+                    ).toList();
+                    final CompletableFuture<Void> futures = CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]));
+                    return CompletableFuture.supplyAsync(() -> {
+                        futures.join();
+                        return employeeDTOS;
+                    });
+                }).join());
+    }
 }
